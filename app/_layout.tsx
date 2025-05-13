2290cc8ecfd825505c374ab5f61aed2beafe91ee
iPexzo <Pexzoking@gmail.com>
2025-05-13 15:11:05 +0300

    Task Navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,35 @@
 import { Stack } from "expo-router";
+import { Button } from "react-native";
+import { Header } from "react-native/Libraries/NewAppScreen";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{ title: "Home", headerShown: false }}
+      />
+      <Stack.Screen
+        name="trips/[Tripid]"
+        options={{
+          title: " Card List",
+          headerBackTitle: "GO Back",
+          headerStyle: { backgroundColor: "red" },
+          headerTitleStyle: { Color: "white" },
+          headerRight: () => {
+            return (
+              <Button
+                title="Edit"
+                onPress={() => {
+                  alert("Only for Admin");
+                }}
+              />
+            );
+          },
+        }}
+      />
+    </Stack>
+  );
 }
 
 export const unstable_settings = {
