f4b402d865328fda95f6cefb3588249bb87d0eb1
Ali Abdullah <aliyabdulah@gmail.com>
2025-05-21 13:11:44 +0300

    Submitting Navigation Task, Where i have made all the pictures redurected to their details page, First step was to create a trip folder and add the trip details inside it, then i have renamed the file to [slug], after added the useLocalSearchParams and a find function. after that i went to the card and added a<Link>

--- app/trips/[slug].tsx
+++ app/trips/[slug].tsx
@@ -1,9 +1,13 @@
 import { Image, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import trips from "@/data/trips";
+import { useLocalSearchParams } from "expo-router";
 
 const TripDetails = () => {
-  const trip = trips[0];
+  const { slug } = useLocalSearchParams();
+  console.log("slug is:", slug);
+  const trip = trips.find((t) => t.slug === slug);
+  // const trip = trips[0];
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{trip?.name}</Text>
